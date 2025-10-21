fe4b81c90ae35ec54642f4c62b803b93cd373743
mridul3301 <mridulsharma3301@gmail.com>
2025-10-21 15:25:46 +0545

    change border

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -9,9 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-       "rounded-lg border bg-card text-card-foreground shadow-sm",
-      "border-black dark:border-white/40",
-      "shadow-black/20 dark:shadow-white",
+       "rounded-lg border-sm bg-card text-card-foreground shadow-sm",
       className
     )}
     {...props}
